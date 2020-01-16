9c4617517f7b066f6baa502e687f6d5cb9ca71db
funkyHS <hs1024942667@163.com>
2020-01-16 11:10:23 +0800

    简易版的新闻应用练习

---
+++ FragmentBestPractice/app/src/main/java/com/company/funky/fragmentbestpractice/News.java
@@ -0,0 +1,21 @@
+package com.company.funky.fragmentbestpractice;
+
+public class News {
+    private String title; // 新闻标题
+    private String content; // 新闻内容
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String title) {
+        this.content = title;
+    }
+
+}
